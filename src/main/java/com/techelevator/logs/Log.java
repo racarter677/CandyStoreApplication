f64955b2518c12687ef0c2c0a959e6fa54ea9df9
Daniel Leitnick <daniel.leitnick@gmail.com>
2023-06-09 09:06:42 -0400

    Update 4: Working on logger.

--- src/main/java/com/techelevator/logs/Log.java
+++ src/main/java/com/techelevator/logs/Log.java
@@ -1,11 +1,18 @@
 package com.techelevator.logs;
 
 import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Log {
 
-    public void write(BigDecimal startBalance, BigDecimal endingBalance) {
+    List<String> logList = new ArrayList<>();
+    private DateTimeFormatter dTFormat = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM); // use MEDIUM LocalDateTime or FULL if using Zoned
 
+    public void write(BigDecimal startBalance, BigDecimal endingBalance) {
+        BigDecimal moneyReceived = endingBalance.subtract(startBalance);
     }
 
 }
